0992782a43752eaf4eb2d72878e29b790f9728a0
nicknish09 <nnmarques97@gmail.com>
2019-06-30 12:39:42 -0300

    active_admin instalado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def authenticate_admin!
+    if user_signed_in?
+      redirect_to new_user_session_path unless current_user.is_admin?
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
